f0e601fad476e7e787070b436140e245208e72ab
s_i_t_h_02 <144014108+Kubczor@users.noreply.github.com>
2025-03-13 10:29:37 +0100

    Update slider.cy.js

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -1,18 +1,46 @@
+// describe('Swiper Gallery Test', function () {
+//   it('Checks if second slide contains "United Kingdom"', function () {
+//     cy.visit('http://localhost:3000');
+//     cy.get('.swiper-button-next').click();
+//     cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+//   });
+// });
+
+// describe('Swiper Gallery Test', function () {
+//   it('Checks if third slide contains "Paris"', function () {
+//     cy.visit('http://localhost:3000');
+//     cy.get('.swiper-button-next').click();
+//     cy.wait(2000);
+//     cy.get('.swiper-button-next').click({ force: true });
+//     cy.wait(2000);
+//     cy.get('.swiper-slide-active').should('contain', 'Paris');
+//   });
+// });
+
 describe('Swiper Gallery Test', function () {
-  it('Checks if second slide contains "United Kingdom"', function () {
+  it('Checks if slider works', function () {
     cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
-    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.wait(2000);
+    cy.get('h1').should('contain.text', 'Rome');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('h1').should('contain.text', 'London');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('h1').should('contain.text', 'Paris');
   });
 });
 
 describe('Swiper Gallery Test', function () {
-  it('Checks if third slide contains "Paris"', function () {
+  it('Checks if slider works with next and prev buttons', function () {
     cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.wait(2000);
-    cy.get('.swiper-button-next').click({ force: true });
+    cy.get('.swiper-slide-active').should('not.have.class', 'swiper-slide-prev'); // Slajd nie powinien mieć klasy 'swiper-slide-prev'
+    cy.get('.swiper-button-prev').click();
     cy.wait(2000);
-    cy.get('.swiper-slide-active').should('contain', 'Paris');
+    cy.get('.swiper-slide-active').should('not.have.class', 'swiper-slide-next'); // Slajd nie powinien mieć klasy 'swiper-slide-next'
   });
 });
+
